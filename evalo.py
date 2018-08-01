7518b18c093c2327ca66159095bfe0728fa80d2a
Alexander Grooff <alexandergrooff@gmail.com>
2018-08-02 00:13:08 +0200

    Improved eval with conde, added run.py

--- evalo.py
+++ evalo.py
@@ -1,13 +1,17 @@
-from kanren import run, eq, var, conde
+from kanren import run, eq, var, vars, conde, goalify, lany
+import ast
+
+typeo = goalify(type)
 
 def evalo(expr, value):
     return eval_expro(expr, [], value)
 
 
 def eval_expro(expr, env, value):
-    return conde([
-        eq(expr, value)
-    ])
-
-x = var()
-print(run(1, x, evalo('1', x)))
+    x, y = vars(2)
+    return conde(
+        (eq(expr, ast.Num(n=x)),  # Numbers
+         lany(typeo(x, int), 
+              typeo(x, float))),
+        (eq(expr,value),),  # Not implemented yet
+    )
---
+++ run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import ast
+from argparse import ArgumentParser
+from kanren import var, run
+
+from evalo import evalo
+
+parser = ArgumentParser(description='Evaluate a python file')
+parser.add_argument('--path', help='Path to the file that should be interpreted')
+parser.add_argument('--expr', '-e', help='Python expression that should be interpreted')
+args = parser.parse_args()
+
+
+def interp(expr):
+    print("Program to be evaluated:\n{}".format(expr))
+    program_ast = ast.parse(expr)
+    print("AST of the program: \n{}".format(ast.dump(program_ast)))
+    x = var()
+    evaluated_program = run(0, x, evalo(program_ast, x))
+    print('Evaluated program: {}'.format(evaluated_program))
+
+
+if args.path:
+    with open(args.path, 'r') as fp:
+        program = fp.read()
+        if program[-1:] == '\n':
+            program = program[:-1]
+        interp(program)
+
+if args.expr:
+    interp(args.expr)
