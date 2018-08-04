532681ec7c44e2b7d3bcab33c041e81f85c3caaa
Alexander Grooff <alexandergrooff@gmail.com>
2018-08-04 10:51:25 +0200

    Use personal fork of unification

--- evalo/evalo.py
+++ evalo/evalo.py
@@ -1,5 +1,6 @@
-from kanren import eq, vars, conde, goalify, isvar, var, run
+from kanren import eq, vars, conde, goalify, isvar, var, run, unifiable
 from kanren.core import EarlyGoalError
+from unification.more import unify_object
 import ast
 
 typeo = goalify(type)
@@ -16,6 +17,20 @@ def eq_obj(x, y):
         raise EarlyGoalError()
 
 
+def eq_objo(u, v):
+    """ Goal such that u == v
+    See also:
+        unify_object
+    """
+
+    def goal_eq(s):
+        result = unify_object(u, v, s)
+        if result is not False:
+            yield result
+
+    return goal_eq
+
+
 def children_typeo(l, t):
     """ type(x[n]) == t """
     if not isvar(l) and not isvar(t):
@@ -27,15 +42,26 @@ def children_typeo(l, t):
         raise EarlyGoalError()
 
 
+def _reify_object_dict(o, s):
+    obj = object.__new__(type(o))
+    d = reify(o.__dict__, s)
+    if d == o.__dict__:
+        return o
+    obj.__dict__.update(d)
+    return obj
+
+
 def evalo(expr, value):
+    unifiable(ast.AST)
     return eval_expro(expr, [], value)
 
 
 def eval_expro(expr, env, value):
     x, y = vars(2)
     return conde(
-        ((eq_obj, expr, ast.Num(n=value)),  # Numbers
-         (typeo, value, int)),
+        ((eq, expr, ast.Num(n=value)),  # Numbers
+         (typeo, value, int),
+         (typeo, expr, ast.Num)),
         ((eq_obj, expr, ast.Module(body=x)),  # Module
          (typeo, x, list),),
          #everyg(eval_expro, [(e, env, value) for e in x])),
