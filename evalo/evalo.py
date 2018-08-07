6afa5af276487135c2117c6a60e5e83015a56f0a
Alexander Grooff <alexandergrooff@gmail.com>
2018-08-07 22:50:25 +0200

    Separate evals into program, stmt, expr

--- evalo/evalo.py
+++ evalo/evalo.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from kanren import eq, vars, conde, goalify, isvar, var, run, unifiable
+from kanren.arith import add
 from kanren.core import EarlyGoalError, lallfirst, success, fail
 from unification.more import unify_object
 import ast
@@ -13,22 +14,54 @@ def check_if_duplicate_call(call_args_1, call_args_2):
         return True
 
 
-def evalo(expr, value):
+def evalo(program, value):
     unifiable(ast.AST)
-    return eval_expro(expr, [], value)
+    return eval_programo(program, [], value)
 
 
-def eval_expro(expr, env, value, previous_args=None):
-    duplicate = check_if_duplicate_call((expr, env, value), previous_args)
+def eval_programo(program, env, value, previous_args=None):
+    print('Evaluating program {} to {} with env {}'.format(program, value, env))
+    current_args = (program, env, value)
+    duplicate = check_if_duplicate_call(current_args, previous_args)
     if duplicate:
         return fail
 
-    v1 = var('v1')
-    print('Evaluating {} to {} with env {}'.format(expr, value, env))
     return conde(
-        ((eq, expr, ast.Expr(value=v1)),  # Expressions
-         eval_expro(v1, env, value, (expr, env, value))),
-        ((eq, expr, ast.Num(n=value)),  # Numbers
-         (typeo, value, int),
-         (typeo, expr, ast.Num)),
+        ((eval_stmto, program, env, value, previous_args),),  # Change this
+    )
+
+
+def eval_stmto(stmt, env, value, previous_args=None):
+    print('Evaluating stmt {} to {} with env {}'.format(stmt, value, env))
+    current_args = (stmt, env, value)
+    duplicate = check_if_duplicate_call(current_args, previous_args)
+    if duplicate:
+        return fail
+
+    exprbody = var('exprbody')
+    return conde(
+        ((eq, stmt, ast.Expr(value=exprbody)),  # Expressions
+         (eval_expro, exprbody, env, value)),
+    )
+
+
+def eval_expro(expr, env, value, previous_args=None):
+    print('Evaluating expr {} to {} with env {}'.format(expr, value, env))
+    current_args = (expr, env, value)
+    duplicate = check_if_duplicate_call(current_args, previous_args)
+    if duplicate:
+        return fail
+
+    op = var('op')
+    v1 = var('v1')
+    v2 = var('v2')
+    e1 = var('e1')
+    e2 = var('e2')
+    return conde(
+        ((eq, expr, ast.Num(n=value)),),  # Numbers
+        ((eq, expr, ast.BinOp(left=v1, op=op, right=v2)),  # Expressions
+         (add, e1, e2, value),
+         (eq, op, env, ast.Add()),
+         (eval_expro, v1, env, e1, current_args),
+         (eval_expro, v2, env, e2, current_args)),
     )
--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -7,21 +7,19 @@ from evalo.evalo import evalo
 
 class TestExpressions(TestCase):
     def run_expr(self, expr, value, eval_expr=False):
-        results = run(0, expr if eval_expr else value, evalo(expr, value))
+        results = run(2, expr if eval_expr else value, evalo(expr, value))
         print('Evaluated results: {}'.format([ast.dump(x) if isinstance(x, ast.AST) else x for x in results]))
         return results
 
-    def test_number_ast_results_in_var_integer(self):
-        ret = self.run_expr(ast.Num(n=1), var())
+    def test_expression_is_evaluated_to_value(self):
+        ret = self.run_expr(ast.Expr(value=ast.Num(n=1)), var('expected_var'))
         self.assertEqual(ret[0], 1)
 
     def test_number_value_results_in_ast_number(self):
         ret = self.run_expr(var(), 1, eval_expr=True)
         self.assertIsInstance(ret[0], ast.Expr)
-        self.assertIsInstance(ret[1], ast.Num)
         self.assertIsInstance(vars(ret[0])['value'], ast.Num)
-        self.assertEqual(vars(ret[1]), {'n': 1})
 
-    def test_expression_is_evaluated_to_value(self):
-        ret = self.run_expr(ast.Expr(value=ast.Num(n=1)), var('expected_var'))
-        self.assertEqual(ret[0], 1)
+    def test_ast_addition_results_in_var_integer(self):
+        ret = self.run_expr(ast.Expr(value=ast.BinOp(left=ast.Num(n=1), op=ast.Add(), right=ast.Num(n=1))), var())
+        self.assertEqual(ret[0], 2)
